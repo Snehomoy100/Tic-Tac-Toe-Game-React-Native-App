6198a4508fc4e88993db619f8945dbcd5b853bc6
snehomoy100 <snehomoy100@gmail.com>
2021-09-23 22:45:23 +0530

    basic styling added for the initial build

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import React from "react";
 
-
 import {
   View,
   TouchableOpacity,
   StyleSheet
 } from 'react-native';
 
-
 import {
   Text,
   Container, // kind of bigger version of View in react-native
   Button,
   H1, H3,
-
+  Header,
+  Body,
+  Card,
+  Content,
 } from 'native-base'
+
+
 
 const App = () => {
   return(
@@ -26,10 +29,32 @@ const App = () => {
   )
 }
 
+
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
+  grid: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 20
+  },
+  box: {
+    width: "33%",
+    marginBottom: 6
+  },
+  card: {
+    height: 120,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  message: {
+    textAlign: "center",
+    textTransform: "uppercase",
+    color: "#FFF",
+    marginTop: 20,
+    backgroundColor: "#4652B3",
+    paddingVertical: 10
   }
+
 });
 
 export default App;
